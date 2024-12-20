e2f1262c071aa876d006a3a419bbe3b6f8396d2b
gomh4417 <134872133+gomh4417@users.noreply.github.com>
2024-12-20 18:01:53 +0900

    merge

--- final/classGear.js
+++ final/classGear.js
@@ -36,8 +36,13 @@ document.addEventListener('DOMContentLoaded', () => {
         { x: 190, y: 280, width: 190, height: 190 }  // canvas3
     ];
 
+<<<<<<< HEAD
     if (window.currentIndex > 3) { //currentIndex가 2일때가 분류 모델 쓰니까
         console.log(currentIndex is ${window.currentIndex}. Model classification will be disabled.);
+=======
+    if (window.currentIndex < 2) { //currentIndex가 2일때가 분류 모델 쓰니까
+        console.log(`currentIndex is ${window.currentIndex}. Model classification will be disabled.`);
+>>>>>>> parent of 87063af (last last fuxking last dance)
         disableModelFunctionality(); // 분류 기능 비활성화
         return; //초기화 진행 중단
     }
@@ -86,8 +91,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
             isModelActive = true;
         }
+<<<<<<< HEAD
         if (window.currentIndex !== 2 || isClassified === 1) {
             console.log(모델 로드 과정 스킵.. ${window.currentIndex} 인덱스가 2인데 이 코드가 계속 뜬다면? 아시죠?);
+=======
+        if (window.currentIndex < 2 || isClassified === 1) {
+            console.log(`모델 로드 과정 스킵.. ${window.currentIndex} 인덱스가 2인데 이 코드가 계속 뜬다면? 아시죠?`);
+>>>>>>> parent of 87063af (last last fuxking last dance)
             return;
         }
     }
